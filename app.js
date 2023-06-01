c5d605c9fb4df027ddee134f9766d88f631c03bb
Danish Shafi <danish.shafi@live.co.uk>
2023-06-01 17:10:29 +0200

    created error messages and storing data

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // Middleware - incoming request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// Creating an error route to 404
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+})
+
+// Returning an error JSON object when 404 triggered
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
